72ad94e50832a6f5bfd33c7bb340c4a6eaeeab0c
Vitor Silva <vitor.gabriel.vinielle@gmail.com>
2023-04-01 17:16:41 -0300

    sequencial para arquivo implementada

--- trabalho-1/main.c
+++ trabalho-1/main.c
@@ -8,6 +8,7 @@
 
 int main()
 {
+    /*
     Encadeada lista;
     FILE *arquivo = leArquivo();
     FILE *output;
@@ -15,7 +16,7 @@ int main()
     char nome[TAM] = "Vitor";
     int rg = 132957;
     criaArquivo();
-     
+        
     printf("Quantidade de linhas: %d\n", nLinhas);
 
     arquivoParaEncadeada(&lista, arquivo, nLinhas);
@@ -28,8 +29,18 @@ int main()
     exibeEncadeada(&lista);
     output = criaArquivo();
     encadeadaParaArquivo(&lista, output);
-    
+
     fclose(output);
     fclose(arquivo);
+    */
+
+    Sequencial lista;
+
+    iniciaSequencial(&lista, 10);
+    preencheSequencial(&lista);
+    exibeSequencial(&lista);
+
+    FILE *output = criaArquivo();
+    sequencialParaArquivo(&lista, output);
     return 0;
 }
