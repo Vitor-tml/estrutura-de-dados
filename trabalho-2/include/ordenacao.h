5879ceb50cf4018aa16ddcc2c40b84b2a4c315e4
Vitor Silva <vitor.gabriel.vinielle@gmail.com>
2023-06-25 02:35:01 -0300

    merge sort com problema

--- trabalho-2/include/ordenacao.h
+++ trabalho-2/include/ordenacao.h
@@ -6,6 +6,8 @@ void selectionSort(Registro **tabela, int tamanho);
 void insertionSort(Registro **tabela, int tamanho);
 void bubbleSort(Registro **tabela, int tamanho);
 void shellSort(Registro **tabela, int tamanho);
+void mergeSort(Registro **tabela, int inicio, int fim);
+void merge(Registro **, int inicio, int meio, int fim);
 void imprimeTabela(Registro **tabela, int tamanho);
 
 #endif
--- trabalho-2/src/ordenacao.c
+++ trabalho-2/src/ordenacao.c
@@ -1,3 +1,5 @@
+#include <math.h>
+#include <stdlib.h>
 #include "ordenacao.h"
 #include "registro.h"
 #include "encadeada.h"
@@ -9,10 +11,10 @@ void selectionSort(Registro **tabela, int tamanho)
     int i, j;
     Registro *aux;
 
-    for(i = 0; i < tamanho; i++)
+    for (i = 0; i < tamanho; i++)
         for (j = i + 1; j < tamanho; j++)
         {
-            if((*tabela[i]).rg > (*tabela[j]).rg)
+            if ((*tabela[i]).rg > (*tabela[j]).rg)
             {
                 aux = tabela[j];
                 tabela[j] = tabela[i];
@@ -27,17 +29,17 @@ void insertionSort(Registro **tabela, int tamanho)
     int i, j;
     Registro *aux;
 
-    for(i = 1; i < tamanho; i ++)
+    for (i = 1; i < tamanho; i++)
     {
         aux = tabela[i];
         j = i - 1;
 
-        while(j >= 0 && aux->rg < (*tabela[j]).rg)
+        while (j >= 0 && aux->rg < (*tabela[j]).rg)
         {
-            tabela[j+1] = tabela[j];
+            tabela[j + 1] = tabela[j];
             j--;
         }
-        tabela[j+1] = aux;
+        tabela[j + 1] = aux;
     }
 }
 
@@ -46,15 +48,15 @@ void bubbleSort(Registro **tabela, int tamanho)
 {
     int i, j;
     Registro *aux;
-    for(j = 0; j < tamanho - 1; j++)
-        for(i = 0; i < tamanho - j - 1; i++)
+    for (j = 0; j < tamanho - 1; j++)
+        for (i = 0; i < tamanho - j - 1; i++)
         {
             aux = tabela[i];
-            if((*tabela[i]).rg > (*tabela[i+1]).rg)
+            if ((*tabela[i]).rg > (*tabela[i + 1]).rg)
             {
                 aux = tabela[i];
-                tabela[i] = tabela[i+1];
-                tabela[i+1] = aux;
+                tabela[i] = tabela[i + 1];
+                tabela[i + 1] = aux;
             }
         }
 }
@@ -66,32 +68,90 @@ void shellSort(Registro **tabela, int tamanho)
     Registro *aux;
 
     int h = 1;
-    while(h < tamanho)
+    while (h < tamanho)
         h = 3 * h + 1;
 
-    while(h > 0)
+    while (h > 0)
     {
-        h = (h - 1)/3;
-        for(i = h; i < tamanho; i++)
+        h = (h - 1) / 3;
+        for (i = h; i < tamanho; i++)
         {
             aux = tabela[i];
             j = i;
 
-            while(j >= h && aux->rg < (*tabela[j - h]).rg)
+            while (j >= h && aux->rg < (*tabela[j - h]).rg)
             {
                 tabela[j] = tabela[j - h];
-                j-=h;
+                j -= h;
             }
             tabela[j] = aux;
         }
     }
 }
 
+// Divide o vetor 2 vezes recursivamente até ser apenas uma célula, depois ordena cada vetor menor entre si
+void mergeSort(Registro **tabela, int inicio, int fim) 
+{
+    int meio;
+    if (inicio < fim)
+    {  
+        meio = floor((inicio + fim) / 2);
+        mergeSort(tabela, inicio, meio);
+        mergeSort(tabela, meio + 1, fim);
+        merge(tabela, inicio, meio, fim);
+    }
+}
+
+// Junta dois vetores ordenadamente
+void merge(Registro **tabela, int inicio, int meio, int fim) 
+{   
+    Registro **aux;
+    int tamanho = fim - inicio + 1;
+    int i = inicio;   // Índice do primeiro vetor
+    int j = meio + 1; // Íncide do segundo vetor
+    int l;
+    int fimI = 0;
+    int fimJ = 0;
+    aux = (Registro **) malloc(tamanho * sizeof(Registro *));
+    if (aux == NULL)
+    {
+        printf("Erro na alocação no MergeSort,\n");
+        exit(1);
+    }
+
+    for (l = 0; l < tamanho; l++)
+    {
+        if(!fimI && !fimJ) // Se nenhum vetor acabou
+        {
+            if ((*tabela[i]).rg < (*tabela[j]).rg) // Coloca o menor valor ainda não colocado entre os dois vetores
+                aux[l] = tabela[i++];
+            else
+                aux[l] = tabela[j++];
+
+            if (i > meio) // Primeiro vetor acabou?
+                fimI = 1;
+            if (j > fim) // Segundo vetor acabou?
+                fimJ = 1;
+        }
+        else // Caso algum vetor acabou
+        {
+            if(!fimI) // Se o primeiro não acabou
+            {
+                printf("i = %d j = %d tamanho = %d aux[l]= %d\n", i, j, meio, l);
+                aux[l] = tabela[i++];
+            }
+            else
+                aux[l] = tabela[j++];
+        }
+    }
+    for(i = 0, j = inicio; i < tamanho; i++, j++)
+        tabela[j] = aux[i];
+}
+
 void imprimeTabela(Registro **tabela, int tamanho)
 {
     int i;
 
-    for(i = 0; i < tamanho; i++)
+    for (i = 0; i < tamanho; i++)
         printf("|%3d|Nome: %-10s|RG: %8d|\n", i, (*tabela[i]).nome, (*tabela[i]).rg);
-
 }
