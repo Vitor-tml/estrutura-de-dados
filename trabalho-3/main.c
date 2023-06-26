97ae9b49c8f40c86d2c9e4e7d5a0e5243869df47
Vitor Silva <vitor.gabriel.vinielle@gmail.com>
2023-06-26 15:21:23 -0300

    Recomecando

--- trabalho-3/main.c
+++ trabalho-3/main.c
@@ -1,33 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "cadeia.h"
+#include <string.h>
+#define MAX_PALAVRA 100
 
-int main()
+typedef struct no{
+    char palavra[MAX_PALAVRA];
+    int tamanho;
+}Dicionario;
+
+int contaPalavras(FILE *arquivo);
+
+void main()
 {
-    int loop, escolha;
-    FILE *arquivo, *invertido;
-    
-    do{
-        printf("O que deseja fazer?\n");
-        printf("1)Ler arquivo texto\n");
-        printf("2)Apresentar o arquivo invertido\n");
-        printf("3)Procurar no arquivo invertido\n");
-        printf("4)Procurar ocorrencia\n\n");
-        scanf("%d", &escolha);
+    FILE *arquivo = fopen("./data/lerolero.txt", "r");
+    Dicionario *arquivoInvertido;
 
-        switch (escolha)
-        {
-            case 1:
-                arquivo = leArquivo();
-                break;
-            
-            case 2:
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
 
-            default:
-                break;
-        }
+    arquivoInvertido = malloc(contaPalavras(arquivo) * sizeof(Dicionario));
 
-        
-    }while(loop != 3);
+}
 
+int contaPalavras(FILE *arquivo)
+{
+    int i = 0;
+    char palavra[MAX_PALAVRA];
+    while (fscanf(arquivo, "%s", palavra) != EOF) {
+        i++;
+    }
+    fseek(arquivo, 0, SEEK_SET);
+    return i;
+}
+
+void preencheDicionario(FILE *origem, Dicionario *destino)
+{
+    char palavra[MAX_PALAVRA];
+    int i = 0, j, existe;
+    while (fscanf(origem, "%s", palavra) != EOF) {
+        for(j = 0; j < i; j++)
+            if(strcmp(destino[i].palavra, palavra) && strstr(destino[i].palavra, palavra))
+                existe = 1;
+        if(!existe)
+            strcpy(destino[i++].palavra, palavra);
+        existe = 0;
+    }
 }
